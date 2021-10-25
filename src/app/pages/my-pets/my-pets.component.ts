41475249a8fccf54ac80d9a46aa2e5b785008d07
Jhon <jhon.ichaccaya.27@unsch.edu.pe>
2021-10-24 21:43:53 -0500

    add detail

--- src/app/pages/my-pets/my-pets.component.ts
+++ src/app/pages/my-pets/my-pets.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FavoritePet } from 'src/app/interfaces/FavoritePet.interface';
+import { Pet } from 'src/app/interfaces/Pet.interface';
 import { User } from 'src/app/models/User.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { FavoritesService } from 'src/app/services/favorites.service';
@@ -59,6 +60,35 @@ export class MyPetsComponent implements OnInit {
         });
         this.ngOnInit();
       }
+    });
+  }
+
+  showDetail(pet: Pet) {
+    Swal.fire({
+      html:
+        '<mat-card style="max-width: 400px;" class="example-card">' +
+        '<mat-card-header>' +
+        '<div mat-card-avatar class="example-header-image"></div>' +
+        '<strong>' +
+        pet.name +
+        '</strong><br>' +
+        '<mat-card-subtitle>' +
+        pet.age +
+        '</mat-card-subtitle>' +
+        '</mat-card-header>' +
+        '<img mat-card-image style="width: 400px;" src="' +
+        pet.image_url +
+        '" alt="Photo of a Shiba Inu">' +
+        '<mat-card-content>' +
+        '<p>' +
+        pet.description +
+        '</p>' +
+        ' </mat-card-content>' +
+        '</mat-card>',
+      showCloseButton: false,
+      showCancelButton: false,
+      showConfirmButton: false,
+      focusConfirm: false,
     });
   }
 }
